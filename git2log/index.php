50ed9d7ce5c5ba47aa3ba061cd81eeed863cc664
TimVosch <timbox@live.nl>
2016-10-09 20:13:52 +0200

    Setup base for git2log

---
+++ git2log/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Aqua - Git2Log</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+    <!-- Main body -->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-9 col-md-offset-1" style="padding-top: 50px;">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <span><strong>Git2Log</strong>, welcome!</span>
+                        <span class="pull-right"><a href="/" style="color: #fff"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a></span>
+                    </div>
+                    <div class="panel-body">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Load Jquery slim -->
+    <script src="http://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
+    <!-- Load bootstrap js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
