c073e75933d63d68d82044bd35d691d07e487cb2
TimVosch <timbox@live.nl>
2016-10-10 19:35:15 +0200

    Fix 500 error (getting real tired now)

--- git2log/database/database.php
+++ git2log/database/database.php
@@ -1,8 +1,8 @@
 <?php
 // Load composer dependencies
-require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'].'/git2log'.'/vendor/autoload.php';
 // Load configuration file
-include $_SERVER['DOCUMENT_ROOT'].'/config.php';
+include $_SERVER['DOCUMENT_ROOT'].'/git2log'.'/config.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
