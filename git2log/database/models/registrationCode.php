3e04523ad49124259e0079d7e6d440daf23dca75
TimVosch <timbox@live.nl>
2016-10-11 21:09:15 +0200

    Add RegistrationCode model base

---
+++ git2log/database/models/registrationCode.php
@@ -0,0 +1,7 @@
+<?php
+namespace aquaweb\database\models;
+include_once $_SERVER['DOCUMENT_ROOT'].'/git2log/database/database.php';
+
+class RegistrationCode extends Illuminate\Database\Eloquent\Model
+{
+}
